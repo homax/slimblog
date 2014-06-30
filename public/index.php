46d2e8fb1c50f35d651424d0388c6438a79515e6
Maxim Kutyrew (Job) <homaxweb@mail.ru>
2014-06-30 12:45:41 +0400

    Second commit add bower add bootstrap directory theme html5-boilerplate template

--- public/index.php
+++ public/index.php
@@ -2,7 +2,12 @@
 require "../vendor/autoload.php";
 
 $app = new \Slim\Slim();
-$app->get('/', function () {
-    echo "Hello, friend!";
+$view = $app->view();
+$view->setTemplatesDirectory('./app/view/bootstrap');
+$app->get('/', function () use ($app) {
+    $title = "Блог на основе Slim Framework v2.4.3";
+    $app->render('../header.vhtml', array('title' => $title));
+    $app->render('index.vhtml');
+    $app->render('../footer.vhtml');
 });
 $app->run();
