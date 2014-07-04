f0a7b7a8d1ced7c6371f821c8375ce1555f990d0
Maxim Kutyrew (Job) <homaxweb@mail.ru>
2014-07-04 09:44:50 +0400

    some changes

--- slimblog_dump.sql
+++ slimblog_dump.sql
@@ -11,20 +11,19 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- Дамп структуры для таблица slimblog.articles
-DROP TABLE IF EXISTS `articles`;
 CREATE TABLE IF NOT EXISTS `articles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT '0',
   `text` text,
-  `category_id` int(11) DEFAULT '0',
+  `categories_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_articles_categories` (`category_id`),
-  CONSTRAINT `FK_articles_categories` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
+  KEY `FK_articles_categories` (`categories_id`),
+  CONSTRAINT `FK_articles_categories` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы slimblog.articles: ~5 rows (приблизительно)
+-- Дамп данных таблицы slimblog.articles: ~4 rows (приблизительно)
 /*!40000 ALTER TABLE `articles` DISABLE KEYS */;
-INSERT INTO `articles` (`id`, `title`, `text`, `category_id`) VALUES
+INSERT INTO `articles` (`id`, `title`, `text`, `categories_id`) VALUES
 	(2, 'Африканская кампания', 'По итогам группового раунда французы оставили очень мощное впечатление: без проблем разделавшись с Гондурасом (3:0), «трехцветные» затем в том же легком стиле забили пять мячей швейцарцам. Был и шестой гол Карима Бензема, но случился он аккурат с финальным свистком. Поэтому точный удар форварда и не нашел отражения в протоколе встречи. В третьем туре команда Дидье Дешама позволила себе нулевую ничью с Эквадором, но эта игра уже ничего не решала.', 1),
 	(3, 'В «Афише» сменился директор по продуктам', 'Арт-директор компании Rambler&Co Михаил Сметана назначен новым директором по продуктам проекта «Афиша». Об этом «Ленте.ру» сообщили в редакции журнала. 15 июля 2014 года он сменит на этом посту Илью Красильщика, который покидает компанию.', 5),
 	(4, 'Злодей дня', 'Магшот — это всем известный по голливудским фильмам снимок подозреваемого в совершении преступления анфас и в профиль, который делается после ареста. Самая что ни на есть протокольная и совсем невеселая вещь, но это если не подойти к ней творчески. Например, на сайте окружной полиции Марикопы, США, посетители могут проголосовать за фото понравившегося им нарушителя закона, выбрав «магшот дня». ', 5),
@@ -34,19 +33,18 @@ INSERT INTO `articles` (`id`, `title`, `text`, `category_id`) VALUES
 
 
 -- Дамп структуры для таблица slimblog.art_tag
-DROP TABLE IF EXISTS `art_tag`;
 CREATE TABLE IF NOT EXISTS `art_tag` (
-  `article_id` int(11) NOT NULL,
-  `tag_id` int(11) NOT NULL,
-  KEY `FK__articles` (`article_id`),
-  KEY `FK_tags` (`tag_id`),
-  CONSTRAINT `FK_tags` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`),
-  CONSTRAINT `FK__articles` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`)
+  `articles_id` int(11) DEFAULT NULL,
+  `tags_id` int(11) DEFAULT NULL,
+  KEY `FK__articles` (`articles_id`),
+  KEY `FK_tags` (`tags_id`),
+  CONSTRAINT `FK_tags` FOREIGN KEY (`tags_id`) REFERENCES `tags` (`id`),
+  CONSTRAINT `FK__articles` FOREIGN KEY (`articles_id`) REFERENCES `articles` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Дамп данных таблицы slimblog.art_tag: ~7 rows (приблизительно)
 /*!40000 ALTER TABLE `art_tag` DISABLE KEYS */;
-INSERT INTO `art_tag` (`article_id`, `tag_id`) VALUES
+INSERT INTO `art_tag` (`articles_id`, `tags_id`) VALUES
 	(6, 2),
 	(2, 4),
 	(5, 1),
@@ -58,14 +56,13 @@ INSERT INTO `art_tag` (`article_id`, `tag_id`) VALUES
 
 
 -- Дамп структуры для таблица slimblog.categories
-DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы slimblog.categories: ~5 rows (приблизительно)
+-- Дамп данных таблицы slimblog.categories: ~4 rows (приблизительно)
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
 INSERT INTO `categories` (`id`, `name`) VALUES
 	(1, 'Спорт'),
@@ -77,7 +74,6 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 
 
 -- Дамп структуры для таблица slimblog.tags
-DROP TABLE IF EXISTS `tags`;
 CREATE TABLE IF NOT EXISTS `tags` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
